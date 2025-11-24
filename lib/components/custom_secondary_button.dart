201418c13f562bfbae9c2b30354903d3daf1c2d5
arjunyuvaraj <arjyuvaraj@gmail.com>
2025-11-23 20:55:43 -0500

    Added active refresh, added pwd and email login option, and filtiers for searching

---
+++ lib/components/custom_secondary_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:gr0ve/utilities/context_extensions.dart';
+
+class CustomSecondaryButton extends StatelessWidget {
+  final String label;
+  final GestureTapCallback onTap;
+
+  const CustomSecondaryButton({
+    super.key,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+    final textStyles = context.text;
+
+    return Material(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: context.colors.primary, width: 3),
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(14),
+        onTap: onTap,
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          alignment: Alignment.center,
+          child: Text(
+            label.capitalized,
+            style: textStyles.labelLarge?.copyWith(
+              color: colors.primary,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 0.5,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
