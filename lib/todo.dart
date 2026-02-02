f29514ada9a72f6d9b6b6dd629243c2075af7f50
arjunyuvaraj <arjyuvaraj@gmail.com>
2026-02-01 21:56:42 -0500

    patching sercuity issuesssss x2

---
+++ lib/todo.dart
@@ -0,0 +1,10 @@
+/*
+TODO:
+- Add timestap for refreshing for teacher and buses
+- Make a proper dialog for differientiang between flutter
+new feature
+1. roles per gradde, SPARK, based on role, message in app
+2. "Capstone counclser, SPARK Member, etc."
+  2a. Role title and description 
+3. data analysis to figure out whos doing what
+*/
